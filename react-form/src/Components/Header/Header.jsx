1275f0ee69caa4e3cd84243456369439e300f02c
zahra nuori sorkhani <zahranuori.1375@gmail.com>
2021-12-09 21:46:39 +0330

    create component

--- react-form/src/Components/Header/Header.jsx
+++ react-form/src/Components/Header/Header.jsx
@@ -1,9 +1,14 @@
-// import React from 'react'
-// import './Components/Header/Header'
-// const Header = () =>{
-//     return (
-//     <img src="../Components/Header/img/informaiton.jpg" alt="information"/>
-//     )
-// }
+import React from 'react'
+import './header.style.css'
 
-// export default Header
+import information from  '../Header/img/information.jpg'
+function Header(){
+    return (
+        <div>
+    <img className='Image-header' src={information} alt="information"/>
+    <h1 className='Header-text'>--------------  Part of the information -------------- </h1>
+    </div>
+    )
+}
+
+export default Header
